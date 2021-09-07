e948611176662f5aeb015a73f1d5195ff5cdfbf5
Daniel Hiromoto <66335240+xTechon@users.noreply.github.com>
2021-09-07 14:18:29 -0400

    Create makefile and restructure files

---
+++ Assembler_Pass_1/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    if (argc != 2) { //check to see if correct amnt of arguments entered
+        printf("ERROR: Usage: %s filename\n", argv[0]);
+        return 0;
+    }
+
+
+    return 0;
+}
